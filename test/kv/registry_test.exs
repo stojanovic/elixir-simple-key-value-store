ebe2d07734fb96dd9346da575ff8ed9cf199bfa1
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-04-09 14:10:06 +0200

    Add monitoring

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -9,10 +9,17 @@ defmodule KV.RegistryTest do
   test "spawns buckets", %{registry: registry} do
     assert KV.Registry.lookup(registry, "shopping") == :error
 
-    KV.Registry.create(registry, "shopping")
-    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    KV.Registry.create registry, "shopping"
+    assert {:ok, bucket} = KV.Registry.lookup registry, "shopping"
 
-    KV.Bucket.put(bucket, "milk", 1)
+    KV.Bucket.put bucket, "milk", 1
     assert KV.Bucket.get(bucket, "milk") == 1
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create registry, "shopping"
+    {:ok, bucket} = KV.Registry.lookup registry, "shopping"
+    Agent.stop bucket
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
